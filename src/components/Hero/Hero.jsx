c1654e23cdbcfee75b76b469445c10cfce731e3b
adityadattamallick <adityadatta30@gmail.com>
2025-11-26 23:21:05 +0600

    Modified the writings on the "hero" section and completed the "projects" component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,14 +7,15 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hello, I am Aditya.</h1>
-        <p className={styles.description}>I am blah blah...
-          Lorem ipsum dolor sit amet
-           consectetur adipisicing elit.
-          Molestias ipsam dolores impedit unde
-           perferendis laboriosam suscipit similique,
-           possimus natus distinctio quia officiis
-            voluptate quaerat at repellat. Doloribus
-            officiis qui quis?
+        <p className={styles.description}>
+          I am a tech enthusiat who works with frontend, backend and
+          machine-learning/deep-learning technologies. I completed my bachelor's
+          degree from North South University in Computer Science and
+          Engineering. I learned the computer science fundamentals and core
+          stuff there, such as DSA, computer architechture, computer networking,
+          machine learning, web development. I needed to submit projects for
+          acadmic courses to get an evaluation on my skills there. Also, I
+          worked on a two research projects there on deep-learning.
         </p>
         <a href="mailto:mymail@gmail.com" className={styles.contactBtn}>
           Contact Me
